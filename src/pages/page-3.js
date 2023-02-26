0b5da87871e1afe59792aafd26f39b279f2426b2
Pavel <pavelsergienko7@gmail.com>
2023-02-26 20:24:56 +0200

    adding all our new blog code for gatsby setup

---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ThirdPage = () => {
+  return (<Layout>
+    <h1>HELLO FROM THE THIRD PAGE</h1>
+  </Layout>)
+}
+
+export default ThirdPage
